1f3bf47bc161ced1b0e2b96e346f0060361d9340
Syahreza <syahrezaraditya@gmail.com>
2024-06-30 23:12:29 +0200

    intial commit & wip: testing all methods

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,14 @@ public class Main {
         PrintWriter writer = new PrintWriter(System.out, true);
 
         // Test getRentalsByCustomer with a known customer name
-        System.out.println("Testing getRentalsByCustomer...");
-        int rentalsCount = db.getRentalsByCustomer(writer, "Mary Smith");
-        System.out.println("Number of rentals: " + rentalsCount);
+//        System.out.println("Testing getRentalsByCustomer...");
+//        int rentalsCount = db.getRentalsByCustomer(writer, "Mary Smith");
+//        System.out.println("Number of rentals: " + rentalsCount);
 
+        // Test getDieHardFan
+//        System.out.println("Testing getDieHardFan...");
+//        int dieHardFanCount = db.getDieHardFan(writer);
+//        System.out.println("Number of records for die-hard fan: " + dieHardFanCount);
         // db.closeDBConnection();
 
     }
